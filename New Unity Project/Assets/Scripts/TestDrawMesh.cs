e5feb400c1b4ecce7b8c048bedb417a065a24f1d
TeruyaHisashi <vxd.naoshi.19961205.maro@gmail.com>
2020-08-25 11:55:30 +0900

    一度別プロジェクトにプログラム等を移す

---
+++ New Unity Project/Assets/Scripts/TestDrawMesh.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class TestDrawMesh : MonoBehaviour
+{
+
+    [Header("DrawMeshInstancedIndirectのパラメータ")]
+    [SerializeField]
+    private Mesh m_mesh;
+
+    [SerializeField]
+    private Material m_instanceMaterial;
+
+    [SerializeField]
+    private Bounds m_bounds;
+
+    [SerializeField]
+    private ShadowCastingMode m_shadowCastingMode;
+
+    [SerializeField]
+    private bool m_receiveShadows;
+
+    [SerializeField]
+    private string m_layerName;
+
+    [SerializeField]
+    private Camera m_camera;
+
+
+    [Space(20)]
+    [SerializeField]
+    private int m_instanceCount;
+
+    private ComputeBuffer m_argsBuffer;
+
+    private ComputeBuffer m_positionBuffer;
+
+    private ComputeBuffer m_eulerAngleBuffer;
+
+    private int m_layer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeArgsBuffer();
+        InitializePositionBuffer();
+        InitializeEulerAngleBuffer();
+
+        m_layer = LayerMask.NameToLayer(m_layerName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Graphics.DrawMeshInstancedIndirect(
+            m_mesh,
+            0,
+            m_instanceMaterial,
+            m_bounds,
+            m_argsBuffer,
+            0,
+            null,
+            m_shadowCastingMode,
+            m_receiveShadows,
+            m_layer,
+            m_camera,
+            LightProbeUsage.BlendProbes,
+            null
+        );
+
+    }
+
+    private void InitializeArgsBuffer() {
+
+        uint[] args = new uint[5] { 0, 0, 0, 0, 0 };
+
+        uint numIndices = (m_mesh != null) ? (uint) m_mesh.GetIndexCount(0) : 0;
+
+        args[0] = numIndices;
+        args[1] = (uint)m_instanceCount;
+        
+        m_argsBuffer = new ComputeBuffer(1, args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
+        m_argsBuffer.SetData(args);
+
+    }
+
+    private void InitializePositionBuffer() {
+
+        // xyz:座標   w:スケール
+        Vector4[] positions = new Vector4[m_instanceCount];
+
+        for (int i = 0; i < m_instanceCount; ++i)
+        {
+
+            positions[i].x = Random.Range(-100.0f, 100.0f);
+            positions[i].y = Random.Range(-100.0f, 100.0f);
+            positions[i].z = Random.Range(-100.0f, 100.0f);
+
+            positions[i].w = Random.Range(0.1f, 1f);
+
+        }
+
+        m_positionBuffer = new ComputeBuffer(m_instanceCount, 4 * 4); // 4 * 4 = (floatのbyte）* (要素数)
+        m_positionBuffer.SetData(positions);
+
+        m_instanceMaterial.SetBuffer("positionBuffer", m_positionBuffer);
+
+    }
+
+    private void InitializeEulerAngleBuffer() {
+
+        Vector3[] angles = new Vector3[m_instanceCount];
+
+        for(int i = 0; i < m_instanceCount; ++i) {
+            angles[i].x = Random.Range(-180.0f, 180.0f);
+            angles[i].y = Random.Range(-180.0f, 180.0f);
+            angles[i].z = Random.Range(-180.0f, 180.0f);
+        }
+
+        m_eulerAngleBuffer = new ComputeBuffer(m_instanceCount, 4 * 3);
+        m_eulerAngleBuffer.SetData(angles);
+
+        m_instanceMaterial.SetBuffer("eulerAngleBuffer", m_eulerAngleBuffer);
+
+    }
+
+    // 領域の解放
+    private void OnDisable() {
+
+        if(m_positionBuffer != null)
+            m_positionBuffer.Release();
+        m_positionBuffer = null;
+
+        if(m_eulerAngleBuffer != null)
+            m_eulerAngleBuffer.Release();
+        m_eulerAngleBuffer = null;
+
+        if(m_argsBuffer != null)
+            m_argsBuffer.Release();
+        m_argsBuffer = null;
+
+    }
+
+}
---
+++ New Unity Project/Assets/Shaders/TestInstancedIndirectSurf.shader
@@ -0,0 +1,99 @@
+﻿Shader "Custom/TestInstancedIndirectSurf"
+{
+    Properties
+    {
+        _Color ("Color", Color) = (1,1,1,1)
+        _MainTex ("Albedo (RGB)", 2D) = "white" {}
+        _Glossiness ("Smoothness", Range(0,1)) = 0.5
+        _Metallic ("Metallic", Range(0,1)) = 0.0
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 200
+
+        CGPROGRAM
+        // Physically based Standard lighting model, and enable shadows on all light types
+        #pragma surface surf Standard addshadow fullforwardshadows // 影を描画するためにはaddshadowが必要
+        #pragma multi_compile_instancing    // GPU Instancingを可能にする
+        #pragma instancing_options procedural:setup // setup関数を呼び出す
+
+        sampler2D _MainTex;
+
+        struct Input
+        {
+            float2 uv_MainTex;
+        };
+
+#ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+    StructuredBuffer<float4> positionBuffer;
+    StructuredBuffer<float3> eulerAngleBuffer;
+#endif
+
+        #define Deg2Rad 0.0174532924
+
+        float4x4 eulerAnglesToRottationMatrix(float3 angles) {
+
+            float cx = cos(angles.x * Deg2Rad); float sx = sin(angles.x * Deg2Rad);
+            float cy = cos(angles.z * Deg2Rad); float sy = sin(angles.z * Deg2Rad);
+            float cz = cos(angles.y * Deg2Rad); float sz = sin(angles.y * Deg2Rad);
+
+            return float4x4(
+                cz*cy + sz*sx*sy, -cz*sy + sz*sx*cy, sz*cx, 0,
+                sy*cx, cy*cx, -sx, 0,
+                -sz*cy + cz*sx*sy, sy*sz + cz*sx*cy, cz*cx, 0,
+                0, 0, 0, 1);
+
+        }
+
+
+        void setup() {
+
+        #ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+            float4 data = positionBuffer[unity_InstanceID];
+            float3 angles = eulerAngleBuffer[unity_InstanceID];
+
+            // スケーリング
+            unity_ObjectToWorld._11_21_31_41 = float4(data.w, 0, 0, 0);
+            unity_ObjectToWorld._12_22_32_42 = float4(0, data.w, 0, 0);
+            unity_ObjectToWorld._13_23_33_43 = float4(0, 0, data.w, 0);
+
+            // 回転
+            unity_ObjectToWorld = mul(eulerAnglesToRottationMatrix(angles), unity_ObjectToWorld);
+
+            // 座標
+            unity_ObjectToWorld._14_24_34_44 = float4(data.xyz, 1);
+
+            unity_WorldToObject = unity_ObjectToWorld;
+            unity_WorldToObject._14_24_34 *= -1;
+            unity_WorldToObject._11_22_33 = 1.0f / unity_WorldToObject._11_22_33;
+        #endif
+
+        }
+
+
+        half _Glossiness;
+        half _Metallic;
+        fixed4 _Color;
+
+        // Add instancing support for this shader. You need to check 'Enable Instancing' on materials that use the shader.
+        // See https://docs.unity3d.com/Manual/GPUInstancing.html for more information about instancing.
+        // #pragma instancing_options assumeuniformscaling
+        UNITY_INSTANCING_BUFFER_START(Props)
+            // put more per-instance properties here
+        UNITY_INSTANCING_BUFFER_END(Props)
+
+        void surf (Input IN, inout SurfaceOutputStandard o)
+        {
+            // Albedo comes from a texture tinted by color
+            fixed4 cy = tex2D (_MainTex, IN.uv_MainTex) * _Color;
+            o.Albedo = cy.rgb;
+            // Metallic and smoothness come from slider variables
+            o.Metallic = _Metallic;
+            o.Smoothness = _Glossiness;
+            o.Alpha = cy.a;
+        }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
