0625c28b4cf2042c5d6a16d8cf6ac6f236bf79b0
TeruyaHisashi <vxd.naoshi.19961205.maro@gmail.com>
2020-08-30 15:20:52 +0900

    Boidsアルゴリズムの仮作成

---
+++ Study_DrawMeshInstancedIndirect/Assets/Scripts/Boids.cs
@@ -0,0 +1,216 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Assertions;
+using System.Runtime.InteropServices;
+
+public class Boids : MonoBehaviour
+{
+
+    [Header("DrawMeshInstancedIndirectのパラメータ")]
+    [SerializeField]
+    private Mesh m_mesh;
+
+    [SerializeField]
+    private Material m_material;
+
+    [SerializeField]
+    private Bounds m_bounds;
+
+    [SerializeField]
+    private ShadowCastingMode m_shadowCastingMode;
+
+    [SerializeField]
+    private bool m_receiveShadows;
+
+    [Space(20)]
+    [SerializeField]
+    private int m_instanceCount;
+
+    [Header("力の強さ")]
+    [Header("Boidsモデルのデータ")]
+    [SerializeField]
+    private float m_cohesionForce = 0.008f;
+    [SerializeField]
+    private float m_separationForce = 0.4f;
+    [SerializeField]
+    private float m_alignmentForce = 0.06f;
+
+    [Space(5)]
+    [Header("力の働く距離")]
+    [SerializeField]
+    private float m_cohesionDistance = 0.5f;
+    [SerializeField]
+    private float m_separationDistance = 0.05f;
+    [SerializeField]
+    private float m_alignmentDistance = 0.1f;
+
+    [Space(5)]
+    [Header("力の働く角度")]
+    [SerializeField]
+    private float m_cohesionAngle = Mathf.PI / 2;
+    [SerializeField]
+    private float m_separationAngle = Mathf.PI / 2;
+    [SerializeField]
+    private float m_alignmentAngle = Mathf.PI / 3;
+
+    [Space(5)]
+    [SerializeField]
+    private float m_boundaryForce = 0.001f;
+    [SerializeField]
+    private float m_boundaryRange = 1f;
+
+    [Space(5)]
+    [SerializeField]
+    private float m_maxVelocity = 0.03f;
+
+    [SerializeField]
+    private float m_minVelocity = 0.005f;
+
+    [SerializeField]    
+    private float m_maxForce = 1f;
+
+    [Space(20)]
+    [SerializeField]
+    private ComputeShader m_boidsSimulator;
+
+    private struct BoidsData {
+        public Vector3 position;
+        public Vector3 velocity;
+    }
+
+    private ComputeBuffer m_argsBuffer;
+
+    private ComputeBuffer m_boidsDataBuffer;
+
+    private int m_boidsCalcKernel;
+
+    private int m_deltaTimeID = Shader.PropertyToID("deltaTime");
+
+    private Vector3Int m_groupSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_instanceCount = Mathf.ClosestPowerOfTwo(m_instanceCount);
+
+        Assert.IsNotNull(m_mesh, "メッシュデータが設定されていません");
+        Assert.IsNotNull(m_material, "マテリアルが設定されていません");
+
+        InitializeArgsBuffer();
+        InitializeBoidsDataBuffer();
+        InitializeBoidsSimulator();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // Boidsモデルの計算をここで行う
+        BoidsCalc();
+
+    }
+
+    void LateUpdate() {
+
+        Graphics.DrawMeshInstancedIndirect(
+            m_mesh,
+            0,
+            m_material,
+            m_bounds,
+            m_argsBuffer,
+            0,
+            null,
+            m_shadowCastingMode,
+            m_receiveShadows
+        );
+
+    }
+
+    private void InitializeArgsBuffer() {
+
+        var args = new uint[] { 0, 0, 0, 0, 0 };
+
+        args[0] = m_mesh.GetIndexCount(0);
+        args[1] = (uint)m_instanceCount;
+
+        m_argsBuffer = new ComputeBuffer(1, 4 * args.Length, ComputeBufferType.IndirectArguments);
+
+        m_argsBuffer.SetData(args);
+
+    }
+
+    private void InitializeBoidsDataBuffer() {
+
+        var boidsData = new BoidsData[m_instanceCount];
+
+        for(int i = 0; i < m_instanceCount; ++i) {
+            boidsData[i].position = Random.insideUnitSphere * m_boundaryRange;
+            var velocity = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+            boidsData[i].velocity = velocity.normalized * m_minVelocity; 
+        }
+
+        m_boidsDataBuffer = new ComputeBuffer(m_instanceCount, Marshal.SizeOf(typeof(BoidsData)));
+        m_boidsDataBuffer.SetData(boidsData);
+
+        m_material.SetBuffer("boidsDataBuffer", m_boidsDataBuffer);
+
+    }
+
+    private void InitializeBoidsSimulator() {
+
+        m_boidsCalcKernel = m_boidsSimulator.FindKernel("BoidsCalculation");
+
+        m_boidsSimulator.GetKernelThreadGroupSizes(m_boidsCalcKernel, out uint x, out uint y, out uint z);
+        m_groupSize = new Vector3Int(m_instanceCount / (int)x, (int)y, (int)z);
+
+        m_boidsSimulator.SetFloat("cohesionForce", m_cohesionForce);
+        m_boidsSimulator.SetFloat("separationForce", m_separationForce);
+        m_boidsSimulator.SetFloat("alignmentForce", m_alignmentForce);
+
+        m_boidsSimulator.SetFloat("cohesionDistance", m_cohesionDistance * m_cohesionDistance);
+        m_boidsSimulator.SetFloat("separationDistance", m_separationDistance * m_separationDistance);
+        m_boidsSimulator.SetFloat("alignmentDistance", m_alignmentDistance * m_alignmentDistance);
+
+        m_boidsSimulator.SetFloat("cohesionAngle", m_cohesionAngle);
+        m_boidsSimulator.SetFloat("separationAngle", m_separationAngle);
+        m_boidsSimulator.SetFloat("alignmentAngle", m_alignmentAngle);
+
+        m_boidsSimulator.SetFloat("boundaryForce", m_boundaryForce);
+        m_boidsSimulator.SetFloat("boundaryRange", m_boundaryRange * m_boundaryRange);
+
+        m_boidsSimulator.SetFloat("minVelocity", m_minVelocity);
+        m_boidsSimulator.SetFloat("maxVelocity", m_maxVelocity);
+
+        m_boidsSimulator.SetInt("instanceCount", m_instanceCount);
+
+        m_boidsSimulator.SetFloat("maxForce", m_maxForce);
+
+        m_boidsSimulator.SetBuffer(m_boidsCalcKernel, "boidsData", m_boidsDataBuffer);
+
+    }
+
+    private void BoidsCalc() {
+
+        m_boidsSimulator.SetFloat(m_deltaTimeID, Time.deltaTime);
+        m_boidsSimulator.Dispatch(m_boidsCalcKernel, m_groupSize.x, m_groupSize.y, m_groupSize.z);
+
+    }
+
+    private void OnDisable() {
+
+        if(m_argsBuffer != null)
+            m_argsBuffer.Release();
+
+        m_argsBuffer = null;
+
+        if(m_boidsDataBuffer != null)
+            m_boidsDataBuffer.Release();
+
+        m_boidsDataBuffer = null;
+
+    }
+
+}
---
+++ Study_DrawMeshInstancedIndirect/Assets/Shaders/BoidsShader.shader
@@ -0,0 +1,124 @@
+﻿Shader "Custom/BoidsShader"
+{
+    Properties
+    {
+        _Color ("Color", Color) = (1,1,1,1)
+        _MainTex ("Albedo (RGB)", 2D) = "white" {}
+        _Glossiness ("Smoothness", Range(0,1)) = 0.5
+        _Metallic ("Metallic", Range(0,1)) = 0.0
+
+        _ScaleX("ScaleX", float) = 1
+        _ScaleY("ScaleY", float) = 1
+        _ScaleZ("ScaleZ", float) = 1
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 200
+
+        CGPROGRAM
+        // Physically based Standard lighting model, and enable shadows on all light types
+        #pragma surface surf Standard addshadow fullforwardshadows // 影を描画するためにはaddshadowが必要
+        #pragma multi_compile_instancing    // GPU Instancingを可能にする
+        #pragma instancing_options procedural:setup // setup関数を呼び出す
+
+
+        // Use shader model 3.0 target, to get nicer looking lighting
+        #pragma target 3.0
+
+        sampler2D _MainTex;
+
+        struct Input
+        {
+            float2 uv_MainTex;
+        };
+
+        struct BoidsData {
+            float3 position;
+            float3 velocity;
+        };
+
+        #ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+        StructuredBuffer<BoidsData> boidsDataBuffer;
+        #endif
+
+        float4x4 eulerAnglesToRottationMatrix(float3 angles) {
+
+            float cx = cos(angles.x); float sx = sin(angles.x);
+            float cy = cos(angles.z); float sy = sin(angles.z);
+            float cz = cos(angles.y); float sz = sin(angles.y);
+
+            return float4x4(
+                cz*cy + sz*sx*sy, -cz*sy + sz*sx*cy, sz*cx, 0,
+                sy*cx, cy*cx, -sx, 0,
+                -sz*cy + cz*sx*sy, sy*sz + cz*sx*cy, cz*cx, 0,
+                0, 0, 0, 1);
+
+        }
+
+        fixed _ScaleX;
+        fixed _ScaleY;
+        fixed _ScaleZ;
+
+        void setup() {
+
+        #ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+            float3 position = boidsDataBuffer[unity_InstanceID].position;
+            float3 velocity = boidsDataBuffer[unity_InstanceID].velocity;
+
+            // スケーリング
+            unity_ObjectToWorld._11_21_31_41 = float4(_ScaleX, 0, 0, 0);
+            unity_ObjectToWorld._12_22_32_42 = float4(0, _ScaleY, 0, 0);
+            unity_ObjectToWorld._13_23_33_43 = float4(0, 0, _ScaleZ, 0);
+
+            float3 angle = float3(
+                -asin(velocity.y/length(velocity.xyz)),
+                atan2(velocity.x, velocity.z),
+                0);
+
+            // 回転
+            unity_ObjectToWorld = mul(eulerAnglesToRottationMatrix(angle), unity_ObjectToWorld);
+
+            // 座標
+            unity_ObjectToWorld._14_24_34_44 = float4(position, 1);
+
+            // モデル行列を求める（間違っているかも. . .）
+            // 参考:https://qiita.com/yuji_yasuhara/items/8d63455d1d277af4c270
+            unity_WorldToObject = unity_ObjectToWorld;
+            unity_WorldToObject._14_24_34 *= -1;
+            unity_WorldToObject._11_12_13 = unity_ObjectToWorld._11_21_31;
+            unity_WorldToObject._21_22_23 = unity_ObjectToWorld._12_22_32;
+            unity_WorldToObject._31_32_33 = unity_ObjectToWorld._13_23_33;
+            unity_WorldToObject._11_12_13 = normalize(unity_WorldToObject._11_12_13);
+            unity_WorldToObject._21_22_23 = normalize(unity_WorldToObject._21_22_23);
+            unity_WorldToObject._31_32_33 = normalize(unity_WorldToObject._31_32_33);
+        #endif
+
+        }
+
+
+        half _Glossiness;
+        half _Metallic;
+        fixed4 _Color;
+
+        // Add instancing support for this shader. You need to check 'Enable Instancing' on materials that use the shader.
+        // See https://docs.unity3d.com/Manual/GPUInstancing.html for more information about instancing.
+        // #pragma instancing_options assumeuniformscaling
+        UNITY_INSTANCING_BUFFER_START(Props)
+            // put more per-instance properties here
+        UNITY_INSTANCING_BUFFER_END(Props)
+
+        void surf (Input IN, inout SurfaceOutputStandard o)
+        {
+            // Albedo comes from a texture tinted by color
+            fixed4 c = tex2D (_MainTex, IN.uv_MainTex) * _Color;
+            o.Albedo = c.rgb;
+            // Metallic and smoothness come from slider variables
+            o.Metallic = _Metallic;
+            o.Smoothness = _Glossiness;
+            o.Alpha = c.a;
+        }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
